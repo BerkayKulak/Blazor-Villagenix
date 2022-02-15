915ecd061fda6cdfdb479ecc5af67587f46f2f99
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-16 02:39:09 +0300

    Hotel Amenities

---
+++ Villagenix.Server/Model/BlazorAmenties.cs
@@ -0,0 +1,10 @@
+﻿namespace Villagenix.Server.Model
+{
+    public class BlazorAmenties
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
