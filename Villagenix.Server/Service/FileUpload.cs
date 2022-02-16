ed0783f1242ad016fc6bda14c9e0f05cc5fa1023
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-16 14:18:13 +0300

    Implement File Upload Interface

--- Villagenix.Server/Service/FileUpload.cs
+++ Villagenix.Server/Service/FileUpload.cs
@@ -1,19 +1,72 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
 using Villagenix.Server.Service.IService;
 
 namespace Villagenix.Server.Service
 {
     public class FileUpload:IFileUpload
     {
-        public Task<string> UploadFile(IBrowserFile file)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
         {
-            throw new System.NotImplementedException();
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file.Name);
+                var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                var folderDirectory = $"{_webHostEnvironment.WebRootPath}\\RoomImages";
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages", fileName);
+                var memoryStream = new MemoryStream();
+                await file.OpenReadStream().CopyToAsync(memoryStream);
+
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fs);
+                }
+
+                var fullPath = $"RoomImages/{fileName}";
+
+                return fullPath;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public bool DeleteFile(string fileName)
         {
-            throw new System.NotImplementedException();
+            bool status = false;
+            try
+            {
+                var path = $"{_webHostEnvironment.WebRootPath}\\RoomImages\\{fileName}";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                
+                throw e;
+            }
         }
     }
 }
