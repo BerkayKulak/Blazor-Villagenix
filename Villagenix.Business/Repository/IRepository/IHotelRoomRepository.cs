18b71180262247e91cdfd2cf062319a1499603ba
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-16 10:43:04 +0300

    Hotel Repository Interface

---
+++ Villagenix.Business/Repository/IRepository/IHotelRoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villagenix.Models;
+
+namespace Villagenix.Business.Repository.IRepository
+{
+    public interface IHotelRoomRepository
+    {
+        public Task<HotelRoomDto> CreateHotelRoom(HotelRoomDto hotelRoomDto);
+        public Task<HotelRoomDto> UpdateHotelRoom(int roomId ,HotelRoomDto hotelRoomDto);
+        public Task<HotelRoomDto> GetHotelRoom(int roomId);
+        public Task<IEnumerable<HotelRoomDto>> GetAllHotelRooms();
+        public Task<HotelRoomDto> IsSameNameRoomAlreadyPresent(string name);
+
+    }
+}
