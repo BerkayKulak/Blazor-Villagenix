2841b53e0efc946cbf47c92445bf9eb9d7aa8391
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-17 22:44:44 +0300

    Add Authentication to API

--- Villagenix.API/Program.cs
+++ Villagenix.API/Program.cs
@@ -1,7 +1,10 @@
 using System.Configuration;
+using System.Text;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Villagenix.API.Helper;
 using Villagenix.Business.Repository;
 using Villagenix.Business.Repository.IRepository;
@@ -24,6 +27,34 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
+var appSettingsSection = builder.Configuration.GetSection("APISettings");
+builder.Services.Configure<APISettings>(appSettingsSection);
+
+var apiSettings = appSettingsSection.Get<APISettings>();
+var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidAudience = apiSettings.ValidAudience,
+            ValidIssuer = apiSettings.ValidIssuer,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
 builder.Services.AddScoped<IHotelImagesRepository, HotelImagesRepository>();
@@ -31,8 +62,7 @@ builder.Services.AddScoped<IAmenityRepository, AmenityRepository>();
 builder.Services.AddRouting(option => option.LowercaseUrls = true);
 builder.Services.AddControllers();
 
-var appSettingsSection = builder.Configuration.GetSection("APISettings");
-builder.Services.Configure<APISettings>(appSettingsSection);
+
 
 var app = builder.Build();
 
