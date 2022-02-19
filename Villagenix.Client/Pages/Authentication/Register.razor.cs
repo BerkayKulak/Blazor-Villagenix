c26caea55c7c6d9cca65ea2655824241c1d52ee4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 18:26:14 +0300

    Login Demo

---
+++ Villagenix.Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Villagenix.Client.Service.IService;
+using Villagenix.Models;
+
+namespace Villagenix.Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private UserRequestDTO UserForRegisteration = new UserRequestDTO();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await authenticationService.RegisterUser(UserForRegisteration);
+            if (result.IsRegisterationSuccessful)
+            {
+                IsProcessing = false;
+                navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                IsProcessing = false;
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+        }
+    }
+}
