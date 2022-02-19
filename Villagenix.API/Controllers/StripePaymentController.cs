ff42bb47e0c500adcc0e26d10d9a87d81a81f2b9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 17:46:05 +0300

    Create Stripe Endpoint in API

--- Villagenix.API/Controllers/StripePaymentController.cs
+++ Villagenix.API/Controllers/StripePaymentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+using Villagenix.Models;
 
 namespace Villagenix.API.Controllers
 {
@@ -14,9 +16,56 @@ namespace Villagenix.API.Controllers
             _configuration = configuration;
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public async Task<IActionResult> Create(StripePaymentDTO payment)
         {
-            return View();
+            try
+            {
+                var domain = _configuration.GetValue<string>("HiddenVilla_Client_URL");
+
+                var options = new SessionCreateOptions
+                {
+                    PaymentMethodTypes = new List<string>
+                    {
+                        "card"
+                    },
+                    LineItems = new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                UnitAmount=payment.Amount*100,//convert to cents
+                                Currency="usd",
+                                ProductData= new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = payment.ProductName
+                                }
+                            },
+                            Quantity=1
+                        }
+                    },
+                    Mode = "payment",
+                    SuccessUrl = domain + "/success-payment?session_id={{CHECKOUT_SESSION_ID}}",
+                    CancelUrl = domain + payment.ReturnUrl
+                };
+
+                var service = new SessionService();
+                Session session = await service.CreateAsync(options);
+
+                return Ok(new SuccessModel()
+                {
+                    Data = session.Id
+                });
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = e.Message
+                });
+            }
         }
     }
 }
