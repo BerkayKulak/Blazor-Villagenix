09946ad4261d9ee2c31ff57ba60c80822b38b33d
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 18:32:08 +0300

    Logout Demo

---
+++ Villagenix.Client/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Villagenix.Client.Service.IService;
+
+namespace Villagenix.Client.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthenticationService authenticationService { get; set; }
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await authenticationService.Logout();
+            navigationManager.NavigateTo("/");
+        }
+    }
+}
