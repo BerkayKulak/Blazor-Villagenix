0b8a107ad956b8d549ebdc532326f9493c948301
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 17:55:57 +0300

    Invoke Stripe

---
+++ Villagenix.Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,6 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe('pk_test_51IuZTULjHP9VtxkS8sqmlLzWmiB66yLi7CCVqHhfKJr7AlWoxyjQI14uzqe69G9eKYeJb2vONsJLomIStkbIl7hE00WXmhYdEr');
+    stripe.redirectToCheckout({
+        sessionId: sessionId
+    });
+};
