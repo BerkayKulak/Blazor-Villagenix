664f19dbb9644f64b4a91c3cb33769be5033a1b1
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 17:43:20 +0300

    Stripe API Endpoint

---
+++ Villagenix.API/Controllers/StripePaymentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Villagenix.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StripePaymentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripePaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
